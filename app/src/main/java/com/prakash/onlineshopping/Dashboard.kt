54e73345cace0cf8612781d3ade5b481a59d14f3
“[prakash <[rawalprakash722@gmail.com]>
2021-01-21 20:04:23 +0545

    dashboard

--- app/src/main/java/com/prakash/onlineshopping/Dashboard.kt
+++ app/src/main/java/com/prakash/onlineshopping/Dashboard.kt
@@ -6,7 +6,7 @@ import android.os.Bundle
 class Dashboard : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-       
+        setContentView(R.layout.activity_dashboard)
 
 
 
